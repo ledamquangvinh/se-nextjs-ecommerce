582a8ef0b963c21488e1ef29d8cd8282187b8e0d
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2024-01-09 11:47:21 +0000

    feat: finished page product detail, onworking with file useCart.tsx

--- app/product/[productId]/ProductDetail.tsx
+++ app/product/[productId]/ProductDetail.tsx
@@ -1,10 +1,11 @@
 "use client"
+
 import Button from "@/app/components/products/Button";
+import ProductImage from "@/app/components/products/ProductImages";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
 import React, { useCallback } from "react";
-
 
 interface ProductDetailProps {
   product: any
@@ -72,9 +73,9 @@ const ProductDetail : React.FC<ProductDetailProps> = ({ product }) => {
 
   return ( 
   <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-    <div>
-      Images
-    </div>
+    <ProductImage cartProduct={cartProduct} product={product} handleColorSelect={handleColorSelect}>
+
+    </ProductImage>
     <div className="flex flex-col gap-1 text-slate-500 text-sm">
       <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
       <div className="flex items-center gap-2">
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/components/Container";
 import { product } from "@/utils/product"
 import ProductDetail from "./ProductDetail";
+import ListRating from "./ListRating";
 interface IParams {
   productId? : string
 }
@@ -11,9 +12,13 @@ const Product = ({params} :{params: IParams}) => {
   return ( 
    <div className="p-8">
     <Container >
-      <ProductDetail product = {product}>
-
-      </ProductDetail>
+      <ProductDetail product = {product}/>
+      <div className="flex flex-col mt-20 gap-4">
+        <div>Add Rating</div>
+        <div>
+          <ListRating product={product}/>
+        </div>
+      </div>
     </Container>
    </div>
   );
